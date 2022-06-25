4f0e0e70461614f21e7fd2e886da039fb259df65
RichLii <lichi0623@gmail.com>
2022-06-25 15:22:59 +0800

    增加github 專案名稱

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/<repo名稱>/'
-        : '/'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Vue-Component-Knob-Checkbox/'
+    : '/'
+
 })
